7bf2e68d965343e8edf5aa892538f70e44198ac2
quguiyou <quguiyou1076@qq.com>
2024-06-02 15:43:52 +0800

    第1.12 分区解析_解析扩展分区数量

--- xdisk.c
+++ xdisk.c
@@ -30,6 +30,31 @@ xfat_err_t xdisk_write_sector(xdisk_t* disk, u8_t* buffer, u32_t start_sector, u
 	return disk->driver->write_sector(disk, buffer, start_sector, count);
 }
 
+static xfat_err_t disk_get_extend_part_count(xdisk_t* disk, u32_t start_sector, u32_t* count) {
+	int r_count = 0;
+	u32_t ext_start_sector = start_sector;
+	u8_t* disk_buffer = temp_buffer;
+	mbr_part_t* part;
+	do {
+		int err = xdisk_read_sector(disk, disk_buffer, start_sector, 1);
+		if (err < 0) {
+			return err;
+		}
+		part = ((mbr_t*)disk_buffer)->part_info;
+		if (part->system_id == FS_NOT_VALID) {
+			break;
+		}
+		r_count++;
+		if ((++part)->system_id != FS_EXTEND) {
+			break;
+		}
+		start_sector = ext_start_sector + part->relative_sectors;
+	} while (1);
+
+	*count = r_count;
+	return FS_ERR_OK;
+}
+
 xfat_err_t xdisk_get_part_count(xdisk_t* disk, u32_t* count) {
 	mbr_part_t* part;
 	u8_t* disk_buffer = temp_buffer;
@@ -39,13 +64,32 @@ xfat_err_t xdisk_get_part_count(xdisk_t* disk, u32_t* count) {
 	}
 
 	int r_count = 0;
+	u8_t extend_part_flag = 0;
+	u32_t start_sector[4];
 	part = ((mbr_t*)disk_buffer)->part_info;
 	for (int i = 0; i < MBR_PRIMARY_PART_NR; i++, part++) {
 		if (part->system_id == FS_NOT_VALID) {
 			continue;
 		}
+		else if (part->system_id == FS_EXTEND) {
+			start_sector[i] = part->relative_sectors;
+			extend_part_flag |= 1 << i;
+		}
 		else {
 			r_count++;
+		}
+	}
+	if (extend_part_flag) {
+		for (int i = 0; i < MBR_PRIMARY_PART_NR; i++) {
+			if (extend_part_flag & (1 << i)) {
+				int ext_count = 0;
+				err = disk_get_extend_part_count(disk, start_sector[i], &ext_count);
+				if (err < 0) {
+					return err;
+				}
+
+				r_count += ext_count;
+			}
 		}
 	}
 	*count = r_count;
